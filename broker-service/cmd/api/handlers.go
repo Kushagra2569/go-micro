b3c995a7aa84db0f6be8569b6e6d3a4f332d0d2e
Kushagra Gupta <kushagra.kingg@live.com>
2025-08-01 09:53:55 +0530

    added initial changes for logger service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -39,11 +45,18 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch reqPayload.Action {
 	case "auth":
 		app.authenticate(w, reqPayload.Auth)
+	case "log":
+		app.logItem(w, reqPayload.Log)
 
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
 
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	//create json
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
